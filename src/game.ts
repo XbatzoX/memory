3fb14fb242a75500ca38e93a57f3bf61567cf2fe
BEN <benbasic@outlook.de>
2026-04-10 15:56:15 +0200

    added font and images for food theme

--- src/game.ts
+++ src/game.ts
@@ -21,12 +21,17 @@ if(contentButtonRef){
 
 function initGame():void{
     loadDataFromLocalStorage();
-    checkCodeTheme();
-    checkFoodTheme();
+    checkBodyBackgroundColor();
     const contentHeaderRef = document.getElementById('game_header');
     if(contentHeaderRef){
         contentHeaderRef.innerHTML = temp.renderHeader(myGameObj.theme);   
     }
+    checkPlayerImages();
+}
+
+function checkBodyBackgroundColor():void{
+    checkCodeThemeBody();
+    checkFoodThemeBody();
 }
 
 function loadDataFromLocalStorage():void{
@@ -34,17 +39,40 @@ function loadDataFromLocalStorage():void{
     if(data){myGameObj = JSON.parse(data);}   
 }
 
-function checkCodeTheme():void{
+function checkCodeThemeBody():void{
     if(myGameObj.theme == 'code_vibes'){
         const contentBodyRef = document.getElementById('body_game');
         if(contentBodyRef){contentBodyRef.classList.add('body-bg-code-theme');}
     }
 }
 
-function checkFoodTheme():void{
+function checkFoodThemeBody():void{
     if(myGameObj.theme == 'food_theme'){
         const contentBodyRef = document.getElementById('body_game');
         if(contentBodyRef){contentBodyRef.classList.add('body-bg-food-theme');}
+    }
+}
+
+function checkPlayerImages():void{
+    checkCodeThemePlayer();
+    checkFoodThemePlayer();
+}
+
+function checkCodeThemePlayer():void{
+    if(myGameObj.theme == 'code_vibes'){
+        const contentImgBlueRef = document.getElementById('icon_blue_player') as HTMLImageElement;
+        const contentImgOrangeRef = document.getElementById('icon_orange_player') as HTMLImageElement;
+        if(contentImgBlueRef){contentImgBlueRef.src = '/assets/img/player_blue_code_theme.svg';}
+        if(contentImgOrangeRef){contentImgOrangeRef.src = '/assets/img/player_orange_code_theme.svg';}
+    }
+}
+
+function checkFoodThemePlayer():void{
+    if(myGameObj.theme == 'food_theme'){
+        const contentImgBlueRef = document.getElementById('icon_blue_player') as HTMLImageElement;
+        const contentImgOrangeRef = document.getElementById('icon_orange_player') as HTMLImageElement;
+        if(contentImgBlueRef){contentImgBlueRef.src = '/assets/img/icon_blue_player_food_theme.svg';}
+        if(contentImgOrangeRef){contentImgOrangeRef.src = '/assets/img/icon_orange_player_food_theme.svg';}
     }
 }
 
--- src/templates.ts
+++ src/templates.ts
@@ -1,14 +1,14 @@
 export function renderHeader(id:string):string{
-    return `<div  class="gameHeader__frame">
+    return `<div  class="gameHeader__frame padding-top-l">
                 <div class="gameHeader__container-${id}">
                     <div class="playerContainer-${id}">
                         <div class="playerContainer-${id}__box">
                             <img id="icon_blue_player" class="playerContainer-${id}__img" src="../assets/img/player_blue_code_theme.svg" alt="blue player icon">
-                            <span id="player_blue_value" class="playerContainer-${id}__valueBlue">0</span>
+                            <span id="player_blue_value" class="playerContainer-${id}__valueBlue padding-top-s">0</span>
                         </div>
                         <div class="playerContainer-${id}__box">
                             <img id="icon_orange_player" class="playerContainer-${id}__img" src="../assets/img/player_blue_code_theme.svg" alt="blue player icon">
-                            <span id="player_orange_value" class="playerContainer-${id}__valueOrange">0</span>
+                            <span id="player_orange_value" class="playerContainer-${id}__valueOrange padding-top-s">0</span>
                         </div>
                     </div>
                 </div>
