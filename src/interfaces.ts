a54fbe490ec0a041db47cb63d22e5563b50b569e
BEN <benbasic@outlook.de>
2026-04-10 10:50:33 +0200

    added icons for code theme

---
+++ src/interfaces.ts
@@ -0,0 +1,17 @@
+export interface Theme{
+    readonly name : string,
+    bg_color : string,
+    header_color : string,
+    current_player_blue : string,
+    current_player_orange : string,
+    player_blue: string,
+    player_orange: string
+    exit_icon : string
+}
+
+export interface Card{
+    frontImage: string,
+    backImage: string,
+    cardNumber: number,
+    colorMatch: string
+}
