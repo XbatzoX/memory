8545397e61dd779f4c3e8862723a461fc46dc0cf
BEN <benbasic@outlook.de>
2026-04-14 15:21:54 +0200

    started with design of game over page

--- src/gameOver.ts
+++ src/gameOver.ts
@@ -1,15 +1,36 @@
 import * as data from './dataObj';
+import * as temp from './templates';
 import { GameResult } from './interfaces';
+import { Settings } from './interfaces';
 
 let resultObj:GameResult;
+let mySettings:Settings;
 
 function init():void{
-    let storageData = localStorage.getItem('myGameResult');
-    if(storageData){resultObj= JSON.parse(storageData);}   
+    getDataFromLocalStorage();
+    createThemeBackground();
     
     console.log(resultObj);
+    console.log(mySettings);
     let testSpan = document.getElementById('test_id');
     if(testSpan){testSpan.innerHTML = resultObj.winner;}
+}
+
+function getDataFromLocalStorage():void{
+    let storageData = localStorage.getItem('myGameResult');
+    if(storageData){resultObj= JSON.parse(storageData);} 
+    storageData = localStorage.getItem('myGameSettings');
+    if(storageData){mySettings = JSON.parse(storageData);}  
+}
+
+function createThemeBackground():void{
+    let contentBodyRef = document.getElementById('body_game_over');
+    if(mySettings.theme == 'code_vibes'){
+        if(contentBodyRef){contentBodyRef.style.backgroundColor = '#303131';}
+    }
+    if(mySettings.theme == 'food_theme'){
+        if(contentBodyRef){contentBodyRef.style.backgroundColor = '#F58E39';}
+    }
 }
 
 if(window.location.pathname.includes('gameOver.html')){
